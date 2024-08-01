fcdc8923b4cbbb4cdc80acf4a07a152fbfb2be99
Oleksandr Poliakov <114873264+VIYD@users.noreply.github.com>
2024-08-01 14:35:27 +0300

    Updated build-with-java-8.sh

--- build-with-java-8.sh
+++ build-with-java-8.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+#
+#This file is used to switch Java version on build machine, as Jenkins uses Java 11, but maven builder requires Java 8.
+
 # Set JAVA_HOME to Java 8
 export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64
 export PATH=$JAVA_HOME/bin:$PATH
